87f271713f323807bfaafb0cf423c3cb08c599f1
Tomas Haas <tomashaas42@gmail.com>
2022-02-06 09:50:19 +0100

    chore: configure logging

--- src/api/app.py
+++ src/api/app.py
@@ -1,7 +1,11 @@
+import logging
+
 import connexion
+import structlog
 
 from .. import services
 from ..container import Container
+from ..settings import Config
 
 
 class PdfRendererAPI:
@@ -13,5 +17,20 @@ class PdfRendererAPI:
         app.add_api("api.yaml")
 
         Container().wire(modules=[services])
+        self._configure_logging(Config().debug)
 
         return app
+
+    @staticmethod
+    def _configure_logging(debug: bool) -> None:
+        level = logging.DEBUG if debug else logging.INFO
+        logging.basicConfig(level=level)
+        structlog.configure_once(
+            processors=[
+                structlog.threadlocal.merge_threadlocal,
+                structlog.processors.add_log_level,
+                structlog.processors.TimeStamper(),
+                structlog.processors.JSONRenderer(),
+            ],
+            logger_factory=structlog.stdlib.LoggerFactory(),
+        )
